3fda6bbfb57cffe45dd942a548eb34911ac3c44b
osami <v.pylayev@mail.ru>
2026-03-10 06:00:54 +0500

    update task

--- internal/model/task.go
+++ internal/model/task.go
@@ -3,17 +3,17 @@ package model
 import "github.com/google/uuid"
 
 type Task struct {
-	ID          uuid.UUID `gorm:"type:uuid" json:"id"`
+	ID          uuid.UUID `gorm:"type:uuid;primaryKey" json:"id"`
 	Type        string    `json:"type"`
-	Descrition  string    `json:"description"`
+	Descrition  string    `gorm:"unique" json:"description"`
 	Decision    string    `json:"decision"`
-	Level       uint      `json:"level"`
+	Level       string    `json:"level"`
 	RightAnswer string    `json:"right_answer"`
 	Likes       uint      `json:"likes"`
 	Dislikes    uint      `json:"dislikes"`
 }
 
-func NewTask(taskType, desc, decision, rightAnswer string, level uint) *Task {
+func NewTask(taskType, desc, decision, rightAnswer string, level string) *Task {
 	return &Task{
 		ID:          uuid.New(),
 		Type:        taskType,
