36a7d585696573ff4de5b3def88236084b695c6e
osami <v.pylayev@mail.ru>
2026-03-17 21:01:10 +0500

    update inc_like handler

---
+++ internal/handler/inc_like.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+)
+
+func (h *Handler) IncLike(c *echo.Context) error {
+	id := c.Param("id")
+
+	if err := h.service.IncLike(c.Request().Context(), id);err != nil{
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.String(http.StatusOK, "success")
+}
