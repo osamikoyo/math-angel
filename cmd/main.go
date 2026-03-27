74fcecc2156ad7a0dc617bd4b695c2f386a2e8de
osami <v.pylayev@mail.ru>
2026-03-27 06:51:42 +0500

    add main.go

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/osamikoyo/math-angel/internal/app"
+)
+
+func main() {
+	path := ""
+	for i, arg := range os.Args {
+		if arg == "--config" {
+			path = os.Args[i+1]
+		}
+	}
+
+	app, err := app.SetupApp(path)
+	if err != nil{
+		log.Println(err)
+
+		return
+	}
+
+	err = app.Run()
+	if err != nil{
+		log.Println(err)
+
+		return
+	}
+}
