f34827c805a3690e839becfdcb5ebebb62427fb8
osami <v.pylayev@mail.ru>
2026-03-28 07:21:55 +0500

    update handler

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"github.com/labstack/echo/v5"
+
+	"github.com/a-h/templ"
 	"github.com/osamikoyo/math-angel/internal/service"
 )
 
@@ -17,6 +19,9 @@ func NewHandler(service *service.Service) *Handler {
 
 func (h *Handler) RegisterRouters(e *echo.Echo) {
 	e.GET("/healthcheck", h.HealthCheck)
+	e.GET("/", h.Home)
+
+	e.Static("/static", "static")
 
 	taskGroup := e.Group("/task")
 
@@ -25,8 +30,13 @@ func (h *Handler) RegisterRouters(e *echo.Echo) {
 	taskGroup.PUT("/inc/dislike", h.IncLike)
 	taskGroup.PUT("/dec/dislike", h.DecDislike)
 
-	
 	taskGroup.GET("/get/bests/:type/level/:level", h.GetBests)
 	taskGroup.GET("/get/task/:id", h.GetTask)
 	taskGroup.GET("/get/random/:type/level/:level", h.GetRandomTask)
+}
+
+func renderWithStatus(c *echo.Context, status int, component templ.Component) error {
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTMLCharsetUTF8)
+	c.Response().WriteHeader(status)
+	return component.Render(c.Request().Context(), c.Response())
 }
