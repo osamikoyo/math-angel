98aec8521b141f304bf39168a1c675bb6a38651c
osami <v.pylayev@mail.ru>
2026-06-15 08:28:22 +0500

    add dec_dislike handler

---
+++ internal/task/handler/dec_dislike.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/osamikoyo/math-angel/internal/errors"
+	"github.com/osamikoyo/math-angel/internal/ui/pages"
+)
+
+func (h *Handler) DecDislike(c *echo.Context) error {
+	id := c.Param("id")
+
+	if err := h.service.DecDislike(c.Request().Context(), id); err != nil {
+		switch err {
+		case errors.ErrBadUID:
+			return c.String(http.StatusBadRequest, err.Error())
+		case errors.ErrNotFound:
+			return c.String(http.StatusNotFound, err.Error())
+		default:
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+	}
+
+	task, err := h.service.GetTask(c.Request().Context(), id)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "error")
+	}
+
+	return renderWithStatus(c, http.StatusOK, pages.DislikeGroup(&pages.Task{
+		ID:    task.UID,
+		Likes: int(task.Likes),
+	}))
+}
