4958bc3bfe62130543ab3123f294611daee27ac3
osami <v.pylayev@mail.ru>
2026-06-23 07:51:15 +0500

    add task_solved handler

---
+++ internal/user/handler/task_solved.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v5"
+)
+
+func (h *Handler) TaskSolved(c *echo.Context) error {
+	idStr := c.Get("user_id")
+	if idStr == nil {
+		return c.String(http.StatusBadRequest, "empty user id")
+	}
+
+	id, err := strconv.Atoi(idStr.(string))
+	if err != nil{
+		return c.String(http.StatusBadRequest, "bad user id")
+	}
+
+	taskIDStr := c.Param("task_id")
+	taskID, err := uuid.Parse(taskIDStr)
+	if err != nil{
+		return c.String(http.StatusBadRequest, "bad task uid")
+	}
+
+	if err = h.service.TaskSolved(c.Request().Context(), uint(id), taskID);err != nil{
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return nil
+}
