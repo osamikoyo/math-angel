f8c9782fada026bc2256e4cfb8e8a6a4e3011494
osami <v.pylayev@mail.ru>
2026-07-02 07:51:39 +0500

    update solution model

--- internal/task/model/solution.go
+++ internal/task/model/solution.go
@@ -5,15 +5,15 @@ import (
 )
 
 type Solution struct {
-	TaskID string `gorm:"primaryKey"`
-	UserID uint
+	TaskID    string `gorm:"primaryKey"`
+	UserID    uint
 	CreatedAt time.Time
 }
 
 func NewSolution(taskID string, userID uint) *Solution {
 	return &Solution{
-		TaskID: taskID,
-		UserID: userID,
+		TaskID:    taskID,
+		UserID:    userID,
 		CreatedAt: time.Now(),
 	}
 }
