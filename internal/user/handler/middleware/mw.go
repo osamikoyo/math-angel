beae3bf79a3dddd9d5eeb526a9f867c563c57ae7
osami <v.pylayev@mail.ru>
2026-07-14 09:48:11 +0500

    update mw

--- internal/user/handler/middleware/mw.go
+++ internal/user/handler/middleware/mw.go
@@ -1,11 +1,19 @@
 package middleware
 
 import (
+	"github.com/a-h/templ"
+	"github.com/labstack/echo/v5"
 	"github.com/osamikoyo/math-angel/internal/user/service"
 )
 
 type Middleware struct {
 	service *service.Service
+}
+
+func renderWithStatus(c *echo.Context, status int, component templ.Component) error {
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMETextHTMLCharsetUTF8)
+	c.Response().WriteHeader(status)
+	return component.Render(c.Request().Context(), c.Response())
 }
 
 func NewMiddleware(svc *service.Service) *Middleware {
