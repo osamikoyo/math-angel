f7c3e2550866ea72dd6cadc59a26d6f9ad224ff4
osami <v.pylayev@mail.ru>
2026-07-19 08:42:43 +0500

    add token

---
+++ internal/user/token/token.go
@@ -0,0 +1,51 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/osamikoyo/math-angel/internal/config"
+)
+
+type Claims struct {
+	UserID string `json:"user_id"`
+	jwt.StandardClaims
+}
+
+func New(userID string, cfg *config.Config) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &Claims{
+		UserID: userID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: jwt.At(time.Now().Add(cfg.Auth.TokenTTL)),
+		},
+	})
+
+	return token.SignedString([]byte(cfg.Auth.JWTSecretKey))
+}
+
+func Validate(tokenString string, cfg *config.Config) (string, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("unexpected signing method")
+		}
+
+		return []byte(cfg.Auth.JWTSecretKey), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok {
+		return "", errors.New("invalid token claims")
+	}
+
+	if err := claims.Valid(&jwt.ValidationHelper{}); err != nil {
+		return "", err
+	}
+
+	return claims.UserID, nil
+}
