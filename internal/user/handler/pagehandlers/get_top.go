eb13a49e11b1420fc5ffcc5e4ea8a9f4b5c1e7b7
osami <v.pylayev@mail.ru>
2026-07-28 11:37:52 +0500

    update get_top handler

--- internal/user/handler/pagehandlers/get_top.go
+++ internal/user/handler/pagehandlers/get_top.go
@@ -1,7 +1,39 @@
 package pagehandlers
 
-import "github.com/labstack/echo/v5"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v5"
+	"github.com/osamikoyo/math-angel/internal/ui/pages"
+)
 
 func (h *PageHandler) GetTop(c *echo.Context) error {
-	
+	pageIndexStr := c.Param("page_index")
+	pageIndex, err := strconv.Atoi(pageIndexStr)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "page_index must be number")
+	}
+
+	pageSizeStr := c.Param("page_size")
+	pageSize, err := strconv.Atoi(pageSizeStr)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "page_size must be number")
+	}
+
+	if pageIndex < 1 {
+		pageIndex = 1
+	}
+	if pageSize < 1 || pageSize > 50 {
+		pageSize = 20
+	}
+
+	top, total, err := h.service.GetUserTop(c.Request().Context(), uint(pageIndex), uint(pageSize))
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	totalPages := (int(total) + pageSize - 1) / pageSize
+
+	return renderWithStatus(c, http.StatusOK, pages.TopPage(top, pageIndex, pageSize, totalPages))
 }
